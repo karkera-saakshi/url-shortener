a6d46d10bf6c54fb13caf0e96b6cc3fa84e2d008
karkera-saakshi <saakshikarkera81@gmail.com>
2026-09-18 20:47:27 +0530

    Close Redis connection, use SIGTERM and SIGINT

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const shortenRoutes = require("./routes/shortenRoutes")
 const redirectRoutes = require("./routes/redirectRoutes")
+const redisClient = require("./utils/redisClient");
 let app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", shortenRoutes);  
-app.use("/", redirectRoutes); 
+app.use("/", redirectRoutes);
+redClient = await redisClient;
+process.on("SIGTERM",()=> {
+    redClient.quit(); // usually sent by Docker, hosting platforms, process managers, etc.
+})
+process.on("SIGINT",()=> {
+    redClient.quit(); // usually when you press Ctrl + C in the terminal.
+    console.log("Ended");
+})
 app.listen(9000, () =>{console.log("Server is running on port 9000")})
