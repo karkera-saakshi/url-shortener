4e616a1d2324cb7b58545e84c640f8eec6b1420a
karkera-saakshi <saakshikarkera81@gmail.com>
2026-09-19 18:36:30 +0530

    Add working frontend

--- frontend/frontend-url-shortener/src/App.jsx
+++ frontend/frontend-url-shortener/src/App.jsx
@@ -1,17 +1,50 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  const handleShorten = (e) => {
+    if(url === "")
+    {
+      alert("Please enter a URL to shorten.");
+      return;
+    }
+    axios.post("http://localhost:9000/api/createShortUrl", {url})
+    .then((res)=>{
+      setShortUrl(res.data);
+    })
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="container">
+      <h1>URL Shortener</h1>
+
+      <label>Enter URL</label>
+
+      <input
+        type="text"
+        placeholder="https://example.com"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+
+      <button onClick={handleShorten}>
+        Convert to Short URL
+      </button>
+
+      <label>Short URL</label>
+
+      <input
+        type="text"
+        value={shortUrl}
+        placeholder="Your short URL will appear here"
+        readOnly
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
